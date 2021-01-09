aa2860b32352680dae63a0ff0211a83a7b908fca
Bruno-Senna <sennasantosbruno@gmail.com>
2021-01-09 11:44:36 -0300

    interpretacao 2

--- quinzena4/lista-de-exercicios/lista.js
+++ quinzena4/lista-de-exercicios/lista.js
@@ -2,5 +2,10 @@
 
 // 1
 
-// Basicamente vai pegar o valor inserio pelo usuario e multiplicar por 100 e imprimeir no console. 
+// Basicamente vai pegar o valor inserio pelo usuario e multiplicar por 100 e imprimir no console. 
+
+// 2
+
+console.log(novoMomtante); saida é "165"
+console.log(segundoMontante); saida é "TIPO DE INVESTIMENTO INFORMADO INCORRETO!"
 
