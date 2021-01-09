44e36628424ea4d952cb5db192d70099c6f0a2f7
Bruno-Senna <sennasantosbruno@gmail.com>
2021-01-09 12:49:25 -0300

    interpretacao 4

--- quinzena4/lista-de-exercicios/lista.js
+++ quinzena4/lista-de-exercicios/lista.js
@@ -11,7 +11,11 @@
 
 // 3
 
-console.log("Quantidade total de números", numeros.length); saida é "Quantidade total de números 14"
-console.log(array1.lenght); saida é "6"
-console.log(array1.lenght); saida é "8"
+// console.log("Quantidade total de números", numeros.length); saida é "Quantidade total de números 14"
+// console.log(array1.lenght); saida é "6"
+// console.log(array1.lenght); saida é "8"
 
+// 4
+
+25  12     12  12      11      11   1       -10     -10
+25, 25 x2, 64, 121 x2, 121 x8, 283, 283 x8, 283 x2, 1590
